a248a7c1c15b73624ccf16c3e4613052d3949c16
m4ck-y <macario.alvaradohdez@gmail.com>
2025-09-19 18:05:01 -0600

    GIN

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/gin"
 	"github.com/m4ck-y/ETL_go/internal/application"
+	"github.com/m4ck-y/ETL_go/internal/infrastructure/services"
 )
 
 func main() {
 	application.Saludar()
+
+	router := gin.Default()
+
+	services.RegisterRoutes(router)
+
+	log.Printf("Servidor escuchando en http://localhost:8080")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v", err)
+	}
 }
